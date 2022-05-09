1226dd546ea20dc19390d34914454280b15265b0
FeverCode <giddylancs.gl@gmail.com>
2022-05-09 17:00:12 +0300

    update email credentials

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     General configuration parent class
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
     # simple mde  configurations
@@ -22,7 +22,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 
 class TestConfig(Config):
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='giddylancs.gl@gmail.com'
-export MAIL_PASSWORD='mmxivkoan9380@giddy'
+export MAIL_USERNAME='lancs@gmail.com'
+export MAIL_PASSWORD='password'
 export SECRET_KEY='secret'
 
 python manage.py server
