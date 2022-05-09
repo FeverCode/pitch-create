9040ee4cb9bca21002b262a605945bfb02c90cf8
FeverCode <giddylancs.gl@gmail.com>
2022-05-09 13:44:15 +0300

    Add Config files

---
+++ config.py
@@ -0,0 +1,57 @@
+import os
+
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch__test'
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+
+}
+
+#  email configurations
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 456
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME='giddylancs.gl@gmail.com'
+export MAIL_PASSWORD='mmxivkoan9380@giddy'
+
+python manage.py server
