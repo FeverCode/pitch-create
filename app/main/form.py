9c0cc243eae4566efa72b0a77271e439ab5b0fd0
FeverCode <giddylancs.gl@gmail.com>
2022-05-09 13:57:35 +0300

    Added views in main

---
+++ app/main/form.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.',
+                        validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    category = SelectField('Category', choices=[('Events', 'Django'), (
+        'Job', 'Flask'), ('Advertisement', 'Angular')], validators=[DataRequired()])
+    post = TextAreaField('Your Pitch', validators=[DataRequired()])
+    submit = SubmitField('Pitch')
+
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment', validators=[DataRequired()])
+    submit = SubmitField('Comment')
