0c6041df5b2218071c37ef723c61c8b85752e026
FeverCode <giddylancs.gl@gmail.com>
2022-05-09 16:02:06 +0300

    Updated Bg image

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ class Config:
     '''
     General configuration parent class
     '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/watchlist'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 export MAIL_USERNAME='giddylancs.gl@gmail.com'
 export MAIL_PASSWORD='mmxivkoan9380@giddy'
+export SECRET_KEY='secret'
 
 python manage.py server
