93194fc4ec382f892c8d40ad13775ee0d90f6688
FeverCode <giddylancs.gl@gmail.com>
2022-05-10 12:36:43 +0300

    updates email

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ config_options = {
 
 #  email configurations
 MAIL_SERVER = 'smtp.googlemail.com'
-MAIL_PORT = 456
+MAIL_PORT = 587
 MAIL_USE_TLS = True
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
