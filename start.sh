b591df46027083778d21be2e741322d369ca0124
FeverCode <giddylancs.gl@gmail.com>
2022-05-10 12:30:11 +0300

    update email

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-export MAIL_USERNAME='lancs@gmail.com'
-export MAIL_PASSWORD='password'
+export MAIL_USERNAME='pitchcreate2022@gmail.com'
+export MAIL_PASSWORD='password2022'
 export SECRET_KEY='GiDdy'
 
 python manage.py server
